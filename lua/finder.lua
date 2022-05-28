49c1be8abe9a730f7afad29319fdec14eba0b621
Cao Duy Lâm <duylamasd@gmail.com>
2022-05-29 04:47:25 +0700

    format telescope

--- lua/finder.lua
+++ lua/finder.lua
@@ -10,8 +10,7 @@ require("telescope").setup {
   }
 }
 
-vim.api.nvim_set_keymap("n", ";f", [[<cmd>Telescope find_files<cr>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", ";r", [[<cmd>Telescope live_grep<cr>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "\\", [[<cmd>Telescope buffers<cr>]], {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", ";;", [[<cmd>Telescope help_tags<cr>]], {noremap = true, silent = true})
-
+vim.api.nvim_set_keymap("n", ";f", [[<cmd>Telescope find_files<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", ";r", [[<cmd>Telescope live_grep<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "\\", [[<cmd>Telescope buffers<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", ";;", [[<cmd>Telescope help_tags<cr>]], { noremap = true, silent = true })
