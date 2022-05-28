b10d78f917c56d15ff7f20e28d9c7089854946b8
Cao Duy Lâm <duylamasd@gmail.com>
2022-05-29 00:10:40 +0700

    customize float border lsp

---
+++ lua/lsp/border.lua
@@ -0,0 +1,21 @@
+vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=yellow guibg=#1f2335]]
+
+local border = {
+  { "╭", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╮", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "╯", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╰", "FloatBorder" },
+  { "│", "FloatBorder" },
+}
+
+local hover_buf = vim.lsp.util.open_floating_preview
+
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return hover_buf(contents, syntax, opts, ...)
+end
