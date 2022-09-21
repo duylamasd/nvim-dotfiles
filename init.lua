5b971a60fc2acdffab42fb2240d2274b55e4f745
Cao Duy Lâm <duylamasd@gmail.com>
2022-09-21 16:16:43 +0700

    use hybrid numbers

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ require("git-blamer")
 require("lsp")
 require("finder")
 
-vim.api.nvim_command("set relativenumber")
+vim.api.nvim_command("set number relativenumber")
 vim.g.vimspector_enable_mappings = "HUMAN"
 vim.api.nvim_command("set termguicolors")
