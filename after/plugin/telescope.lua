87e42f29d3f19eb45af3e2e023e3ef435ddc60d8
Cao Duy Lâm <duylamasd@gmail.com>
2022-10-17 17:29:16 +0700

    add more telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -26,6 +26,11 @@ telescope.setup {
         ["q"] = actions.close
       }
     }
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown"
+    }
   }
 }
 
@@ -35,3 +40,9 @@ vim.keymap.set("n", "ff", builtin.find_files, opts)
 vim.keymap.set("n", "fg", builtin.live_grep, opts)
 vim.keymap.set("n", "fb", builtin.buffers, opts)
 vim.keymap.set("n", "fh", builtin.help_tags, opts)
+
+vim.keymap.set("n", "gc", builtin.git_commits, opts)
+vim.keymap.set("n", "gbc", builtin.git_bcommits, opts)
+vim.keymap.set("n", "gbr", builtin.git_branches, opts)
+
+vim.keymap.set("n", "ts", builtin.treesitter, opts)
