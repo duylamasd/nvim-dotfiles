f2f51f1216999582c426509916b525cb71edc9da
Cao Duy Lâm <duylamasd@gmail.com>
2023-01-02 17:23:15 +0700

    change to moon

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -36,7 +36,7 @@ onedarkpro.setup({
 tokyonight.setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
@@ -44,9 +44,9 @@ tokyonight.setup({
     -- Style to be applied to different syntax groups
     -- Value is any valid attr-list value for `:help nvim_set_hl`
     comments = {italic = true},
-    keywords = {italic = true},
-    functions = {},
-    variables = {},
+    keywords = {italic = true, bold = true},
+    functions = {bold = true},
+    variables = {italic = true},
     -- Background styles. Can be "dark", "transparent" or "normal"
     sidebars = "dark", -- style for sidebars, see below
     floats = "dark" -- style for floating windows
