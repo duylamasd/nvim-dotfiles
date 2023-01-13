34e68f0c0bda9feaddb4bf475b57ca1e0406409b
Cao Duy Lâm <duylamasd@gmail.com>
2023-01-13 13:03:33 +0700

    not using saga

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,42 +1,14 @@
 local ok, configs = pcall(require, "nvim-treesitter.configs")
 
-if not ok then
-  return
-end
+if not ok then return end
 
 configs.setup {
   ensure_installed = {
-    "bash",
-    "c",
-    "cmake",
-    "cpp",
-    "css",
-    "dockerfile",
-    "go",
-    "html",
-    "http",
-    "javascript",
-    "jsdoc",
-    "json",
-    "json5",
-    "lua",
-    "llvm",
-    "make",
-    "python",
-    "rust",
-    "toml",
-    "tsx",
-    "typescript",
-    "vim",
-    "yaml",
-    "scss",
-    "graphql",
-    "prisma",
-    "gomod",
-    "gowork",
+    "bash", "c", "cmake", "cpp", "css", "dockerfile", "go", "html", "http",
+    "javascript", "jsdoc", "json", "json5", "lua", "llvm", "make", "python",
+    "rust", "toml", "tsx", "typescript", "vim", "yaml", "scss", "graphql",
+    "prisma", "gomod", "gowork", "markdown"
   },
   sync_install = true,
-  highlight = {
-    enable = true,
-  }
+  highlight = {enable = true}
 }
--- lua/utils/lsp-on-attach.lua
+++ lua/utils/lsp-on-attach.lua
@@ -10,11 +10,9 @@ local diagnostic = require("lspsaga.diagnostic")
 
 local lsp_formatting = function(bufnr, async)
   vim.lsp.buf.format({
-    filter = function(client)
-      return client.name == "null-ls"
-    end,
+    filter = function(client) return client.name == "null-ls" end,
     bufnr = bufnr,
-    async = async,
+    async = async
   })
 end
 
@@ -31,44 +29,55 @@ local on_attach = function(client, bufnr)
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap = true, silent = true }
+  local opts = {noremap = true, silent = true}
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  --buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  -- buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  --buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<CR>", opts)
-  --buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap("n", "<space>ca", "<cmd>Lspsaga code_action<CR>", opts)
+  buf_set_keymap('n', '<space>wa',
+                 '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr',
+                 '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl',
+                 '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+                 opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+                 opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  -- buf_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<CR>", opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
+                 opts)
+  -- buf_set_keymap("n", "<space>ca", "<cmd>Lspsaga code_action<CR>", opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  --buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-  --buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>',
+                 opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  -- buf_set_keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  -- buf_set_keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>',
+                 opts)
 
-  vim.keymap.set("n", "[e", function() diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, opts)
-  vim.keymap.set("n", "]e", function() diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, opts)
+  vim.keymap.set("n", "[e", function()
+    diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})
+  end, opts)
+  vim.keymap.set("n", "]e", function()
+    diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})
+  end, opts)
 
   if client.supports_method("textDocument/formatting") then
-    vim.keymap.set("n", "<space>f", function() lsp_formatting(bufnr, true) end, opts)
+    vim.keymap.set("n", "<space>f", function() lsp_formatting(bufnr, true) end,
+                   opts)
 
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = augroup,
       buffer = bufnr,
-      callback = function()
-        lsp_formatting(bufnr, false)
-      end,
+      callback = function() lsp_formatting(bufnr, false) end
     })
   end
 end
