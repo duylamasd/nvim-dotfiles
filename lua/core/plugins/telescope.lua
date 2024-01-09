5e7e2a86ece65306b30cf0b0f9998e41dd6cd2e2
Cao Duy Lâm <duylamasd@gmail.com>
2024-01-09 12:47:56 +0700

    add telescope-file-browser

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -5,9 +5,11 @@ M.config = function()
   local actions = require("telescope.actions")
 
   telescope.setup {
-    defaults = { mappings = { n = { ["q"] = actions.close } } },
-    pickers = { find_files = { theme = "dropdown" } }
+    defaults = {mappings = {n = {["q"] = actions.close}}},
+    pickers = {find_files = {theme = "dropdown"}}
   }
+
+  telescope.load_extension "file_browser"
 end
 
 local builtin = require("telescope.builtin")
@@ -17,59 +19,59 @@ M.keys = {
     "ff",
     builtin.find_files,
     desc = "Telescope find files",
-    mode = { "n" },
+    mode = {"n"},
     noremap = true,
     silent = true
   }, {
-  "fg",
-  builtin.live_grep,
-  desc = "Telescope live grep",
-  mode = { "n" },
-  noremap = true,
-  silent = true
-}, {
-  "fb",
-  builtin.buffers,
-  desc = "Telescope buffers",
-  mode = { "n" },
-  noremap = true,
-  silent = true
-}, {
-  "fh",
-  builtin.help_tags,
-  desc = "Telescope help tags",
-  mode = { "n" },
-  noremap = true,
-  silent = true
-}, {
-  "gc",
-  builtin.git_commits,
-  desc = "Telescope git commits",
-  mode = { "n" },
-  noremap = true,
-  silent = true
-}, {
-  "gbc",
-  builtin.git_bcommits,
-  desc = "Telescope git bcommits",
-  mode = { "n" },
-  noremap = true,
-  silent = true
-}, {
-  "gbr",
-  builtin.git_branches,
-  desc = "Telescope git branches",
-  mode = { "n" },
-  noremap = true,
-  silent = true
-}, {
-  "ts",
-  builtin.treesitter,
-  desc = "Telescope treesitter",
-  mode = { "n" },
-  noremap = true,
-  silent = true
-}
+    "fg",
+    builtin.live_grep,
+    desc = "Telescope live grep",
+    mode = {"n"},
+    noremap = true,
+    silent = true
+  }, {
+    "fb",
+    builtin.buffers,
+    desc = "Telescope buffers",
+    mode = {"n"},
+    noremap = true,
+    silent = true
+  }, {
+    "fh",
+    builtin.help_tags,
+    desc = "Telescope help tags",
+    mode = {"n"},
+    noremap = true,
+    silent = true
+  }, {
+    "gc",
+    builtin.git_commits,
+    desc = "Telescope git commits",
+    mode = {"n"},
+    noremap = true,
+    silent = true
+  }, {
+    "gbc",
+    builtin.git_bcommits,
+    desc = "Telescope git bcommits",
+    mode = {"n"},
+    noremap = true,
+    silent = true
+  }, {
+    "gbr",
+    builtin.git_branches,
+    desc = "Telescope git branches",
+    mode = {"n"},
+    noremap = true,
+    silent = true
+  }, {
+    "ts",
+    builtin.treesitter,
+    desc = "Telescope treesitter",
+    mode = {"n"},
+    noremap = true,
+    silent = true
+  }, {"nt", ":Telescope file_browser<CR>", mode = {"n"}, noremap = true}
 }
 
 return M
