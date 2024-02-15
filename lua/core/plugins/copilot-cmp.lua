886c93b3729c478b7509f16b08d62911bc146a6b
Cao Duy Lâm <duylamasd@gmail.com>
2024-02-15 16:08:06 +0700

    feat: add copilot-cmp

---
+++ lua/core/plugins/copilot-cmp.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.config = function(_, opts)
+  local copilot_cmp = require("copilot_cmp")
+  copilot_cmp.setup(opts)
+  -- attach cmp source whenever copilot attaches
+  -- fixes lazy-loading issues with the copilot cmp source
+  require("lazyvim.util").lsp.on_attach(function(client)
+    if client.name == "copilot" then copilot_cmp._on_insert_enter({}) end
+  end)
+end
+
+M.opts = {event = {"InsertEnter", "LspAttach"}, fix_pairs = true}
+
+return M
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,77 +13,83 @@ local telescope = require("core.plugins.telescope")
 local undotree = require("core.plugins.undotree")
 local noice = require("core.plugins.noice")
 local copilot = require("core.plugins.copilot")
+local copilot_cmp = require("core.plugins.copilot-cmp")
 
 return {
-  {"LazyVim/LazyVim", opts = {colorscheme = "tokyonight"}}, {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {"nvim-lua/plenary.nvim"},
-    config = telescope.config,
-    keys = telescope.keys
-  }, {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      {"kyazdani42/nvim-web-devicons", opt = true}, {"kdheepak/tabline.nvim"}
-    },
-    config = lualine.config
-  }, {"lukas-reineke/indent-blankline.nvim", config = ibl.config},
-  {"hrsh7th/vim-vsnip"}, {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {"hrsh7th/cmp-nvim-lsp"}, {"hrsh7th/cmp-buffer"}, {"hrsh7th/cmp-path"},
-      {"hrsh7th/cmp-cmdline"}, {"hrsh7th/cmp-vsnip"}, {"onsails/lspkind-nvim"}
-    },
-    config = cmp.config
-  }, {"editorconfig/editorconfig-vim"}, {"APZelos/blamer.nvim"}, {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = {"VeryLazy"},
-    config = treesitter.config
-  }, {"tpope/vim-fugitive"},
-  {"windwp/nvim-autopairs", config = autopairs.config}, {
-    "williamboman/mason.nvim",
-    dependencies = {
-      {"williamboman/mason-lspconfig.nvim"}, {"hrsh7th/cmp-nvim-lsp"},
-      {"neovim/nvim-lspconfig"}
-    },
-    config = mason.config
-  }, {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {"nvim-lua/plenary.nvim"},
-    config = null_ls.config
-  }, {"mbbill/undotree", keys = undotree.keys},
-  {"folke/tokyonight.nvim", config = tokyonight.config}, {
-    "startup-nvim/startup.nvim",
-    dependencies = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-    config = startup.config
-  }, {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      config = dapui.config,
-      keys = dapui.keys
-    },
-    keys = dap.keys
-  }, {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
-  }, {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    config = noice.config,
-    keys = noice.keys,
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim", -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify"
-    }
-  }, {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = copilot.opts
+  { "LazyVim/LazyVim",                     opts = { colorscheme = "tokyonight" } }, {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = telescope.config,
+  keys = telescope.keys
+}, {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    { "kyazdani42/nvim-web-devicons", opt = true }, { "kdheepak/tabline.nvim" }
+  },
+  config = lualine.config
+}, { "lukas-reineke/indent-blankline.nvim", config = ibl.config },
+  { "hrsh7th/vim-vsnip" }, {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    { "hrsh7th/cmp-nvim-lsp" }, { "hrsh7th/cmp-buffer" }, { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" }, { "hrsh7th/cmp-vsnip" }, { "onsails/lspkind-nvim" }
+  },
+  config = cmp.config
+}, { "editorconfig/editorconfig-vim" }, { "APZelos/blamer.nvim" }, {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "VeryLazy" },
+  config = treesitter.config
+}, { "tpope/vim-fugitive" },
+  { "windwp/nvim-autopairs",        config = autopairs.config }, {
+  "williamboman/mason.nvim",
+  dependencies = {
+    { "williamboman/mason-lspconfig.nvim" }, { "hrsh7th/cmp-nvim-lsp" },
+    { "neovim/nvim-lspconfig" }
+  },
+  config = mason.config
+}, {
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = null_ls.config
+}, { "mbbill/undotree",    keys = undotree.keys },
+  { "folke/tokyonight.nvim", config = tokyonight.config }, {
+  "startup-nvim/startup.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  config = startup.config
+}, {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    config = dapui.config,
+    keys = dapui.keys
+  },
+  keys = dap.keys
+}, {
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+}, {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  config = noice.config,
+  keys = noice.keys,
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",   -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify"
   }
+}, {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = copilot.opts
+}, {
+  "zbirenbaum/copilot-cmp",
+  dependencies = "copilot.lua",
+  opts = copilot_cmp.opts,
+  config = copilot_cmp.config
+}
 }
