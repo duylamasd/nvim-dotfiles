e4d56631571cd3cce295d8eed688c9988a0109f4
Cao Duy Lâm <duylamasd@gmail.com>
2024-03-04 14:51:25 +0700

    feat: add hover lsp config

--- lua/core/plugins/noice.lua
+++ lua/core/plugins/noice.lua
@@ -9,7 +9,8 @@ M.config = function()
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true
-      }
+      },
+      hover = { silent = true }
     },
     presets = {
       bottom_search = false,        -- use a classic bottom cmdline for search
