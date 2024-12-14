16f58a5135bc4d14bd8b12e57d81676f9bc69058
Cao Duy Lâm <duylamasd@gmail.com>
2024-12-14 16:34:50 +0700

    remove unused plugins

--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -27,14 +27,14 @@ local M = {
         -- stylua: ignore
         center = {
           { action = "Telescope find_files",                                    desc = " Find File",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
+          { action = "ene | startinsert",                                       desc = " New File",        icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                                      desc = " Recent Files",    icon = " ", key = "r" },
+          { action = "Telescope live_grep",                                     desc = " Find Text",       icon = " ", key = "g" },
           { action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
-          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          { action = 'lua require("persistence").load()',                       desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                              desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "Lazy",                                                    desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = "qa",                                                      desc = " Quit",            icon = " ", key = "q" },
         },
         footer = function()
           local stats = require("lazy").stats()
