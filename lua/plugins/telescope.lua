df6740455d85dcd3b5113f33afa9a6d5149dcd26
Cao Duy Lam <duylamasd@gmail.com>
2025-05-11 23:59:55 +0700

    fix(telescopee): fix telescope import error

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,3 @@
-local builtin = require("telescope.builtin")
-
 local M = {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
@@ -18,72 +16,77 @@ local M = {
 
     telescope.load_extension("noice")
   end,
-  keys = {
-    {
-      "ff",
-      builtin.find_files,
-      desc = "Telescope find files",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-    {
-      "fg",
-      builtin.live_grep,
-      desc = "Telescope live grep",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-    {
-      "fb",
-      builtin.buffers,
-      desc = "Telescope buffers",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-    {
-      "fh",
-      builtin.help_tags,
-      desc = "Telescope help tags",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-    {
-      "gc",
-      builtin.git_commits,
-      desc = "Telescope git commits",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-    {
-      "gbc",
-      builtin.git_bcommits,
-      desc = "Telescope git bcommits",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-    {
-      "gbr",
-      builtin.git_branches,
-      desc = "Telescope git branches",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-    {
-      "ts",
-      builtin.treesitter,
-      desc = "Telescope treesitter",
-      mode = { "n" },
-      noremap = true,
-      silent = true,
-    },
-  },
+  keys = function()
+    local builtin = require("telescope.builtin")
+
+    local keys = {
+      {
+        "ff",
+        builtin.find_files,
+        desc = "Telescope find files",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+      {
+        "fg",
+        builtin.live_grep,
+        desc = "Telescope live grep",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+      {
+        "fb",
+        builtin.buffers,
+        desc = "Telescope buffers",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+      {
+        "fh",
+        builtin.help_tags,
+        desc = "Telescope help tags",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+      {
+        "gc",
+        builtin.git_commits,
+        desc = "Telescope git commits",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+      {
+        "gbc",
+        builtin.git_bcommits,
+        desc = "Telescope git bcommits",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+      {
+        "gbr",
+        builtin.git_branches,
+        desc = "Telescope git branches",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+      {
+        "ts",
+        builtin.treesitter,
+        desc = "Telescope treesitter",
+        mode = { "n" },
+        noremap = true,
+        silent = true,
+      },
+    }
+    return keys
+  end,
 }
 
 return M
