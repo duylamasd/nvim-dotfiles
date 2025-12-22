35d037d0d31448a590312bd0d45ae681cc583ccf
Cao Duy Lâm <duylamasd@gmail.com>
2025-12-22 12:08:28 +0700

    feat: add gemini-cli plugin

---
+++ lua/plugins/gemini.lua
@@ -0,0 +1,12 @@
+local M = {
+  "marcinjahn/gemini-cli.nvim",
+  cmd = "Gemini",
+  -- Example key mappings for common actions:
+  keys = {},
+  dependencies = {
+    "folke/snacks.nvim",
+  },
+  config = true,
+}
+
+return M
